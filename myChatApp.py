d9c771fbb114d83c90fd801486f2a617a1be6959
kuromi7946 <floatworld@gmail.com>
2026-02-12 00:28:13 +0900

    gemini3f

--- myChatApp.py
+++ myChatApp.py
@@ -17,9 +17,9 @@ SYSTEM_PROMPT = """
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
     model = genai.GenerativeModel(
-        model_name='gemini-2.5-flash-lite',
+        model_name='gemini-3-flash-preview',
         system_instruction=SYSTEM_PROMPT,
-        tools=[{"google_search": {}}]
+#        tools=[{"google_search_retrieval": {}}]
     )
 except Exception as e:
     st.error(f"システム設定エラー: {e}")
