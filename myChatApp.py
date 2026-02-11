788f353a5ae09e0deecb4655f37dfb6de711f37b
kuromi7946 <floatworld@gmail.com>
2026-02-12 00:26:24 +0900

    gemini2.5l

--- myChatApp.py
+++ myChatApp.py
@@ -17,7 +17,7 @@ SYSTEM_PROMPT = """
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
     model = genai.GenerativeModel(
-        model_name='gemini-3-flash-preview',
+        model_name='gemini-2.5-flash-lite',
         system_instruction=SYSTEM_PROMPT,
         tools=[{"google_search_retrieval": {}}]
     )
